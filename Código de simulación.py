dd67376c0f78be2c544cf3b58ae9e4c355750b8f
VicenteAZB <135163471+VicenteAZB@users.noreply.github.com>
2023-06-28 14:00:22 -0400

    Update and rename Código de simulación to Código de simulación.py

--- Código de simulación.py
+++ Código de simulación.py
@@ -4,14 +4,14 @@ import pygame
 pygame.init()
 
 # Dimensiones de la ventana
-width = 1400
-height = 600
+width = 425
+height = 700
 
 # Colores
 white = (255, 255, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
-
+black= (0,0,0)
 # Configuracion de la ventana
 window = pygame.display.set_mode((width, height))
 
@@ -33,7 +33,7 @@ def movimiento_con_friccion(masa, fuerza, coeficiente_roce_estatico, coeficiente
         velocidad = 0
     else:
         # Fuerza aplicada supera la fuerza maxima estatica
-        fuerza_friccion = -coeficiente_roce_dinamico * masa * 9.8  # Fuerza de friccion dinamica
+        fuerza_friccion = -coeficiente_roce_dinamico * masa * 9.8 * 0.95  # Fuerza de friccion dinamica
         aceleracion = (fuerza + fuerza_friccion) / masa
         velocidad = aceleracion * tiempo
 
@@ -43,50 +43,59 @@ def movimiento_con_friccion(masa, fuerza, coeficiente_roce_estatico, coeficiente
     return aceleracion, velocidad, posicion
 
 # Parametros de ejemplo
-masa = 3  # en kg
-fuerza = 20  # en N
-coeficiente_roce_estatico = 0.7
-coeficiente_roce_dinamico = 0.6
-tiempo = 0.5  # en segundos
+masa = float(input("Ingrese masa: "))  # en kg
+fuerza = float(input("Ingrese fuerza: "))  # en N
+coeficiente_roce_estatico = float(input("Ingrese el coeficiente de roce estatico: "))
+coeficiente_roce_dinamico = float(input("Ingrese el coeficiente de roce dinamico: "))
+tiempo = float(input("Ingrese el tiempo: "))  # en segundos
 
 # Posicion inicial
-posicion_x = 887 
-posicion_y = 200   #pelota roja
-posicion_x2 = 215
-posicion_y2 = 200  #pelota verde
+posicion_x = 150 
+posicion_y = 300   #pelota verde
  
 # Triangulo rectangulo
-triangulo = [(20, height - 100), (650, height - 100), (20, height - 500)] #triangulo verde
-triangulo2 = [(770, height - 100), (1158, height - 100), (770, height - 500)]#triangulo rojo
+triangulo = [(20, height - 100), (420, height - 100), (20, height - 500)]#triangulo rojo
+
+def Plano():
+    # Dibujo del triangulo rectangulo
+    pygame.draw.polygon(window, red, triangulo)
+
+def Masa():
+    # Dibujo del objeto (pelota)
+    pygame.draw.circle(window, green, (int(posicion_x), int(posicion_y)), 21)
 
 # Bucle principal del juego
 while True:
+    X = 150 ; Y = 300
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            posicion_x = X ; posicion_y = Y
+ 
 
     # Actualizacion de la posicion y velocidad del circulo verde
-    aceleracion, velocidad, posicion = movimiento_con_friccion(masa, fuerza,
-                                                            coeficiente_roce_estatico,
-                                                            coeficiente_roce_dinamico, tiempo)
-    posicion_y += aceleracion+velocidad+posicion
-    posicion_x += aceleracion+velocidad+posicion
-
-    # Dibujo en la ventana
     window.fill(white)
 
-    # Dibujo del triangulo rectangulo
-    pygame.draw.polygon(window, green, triangulo)
-    pygame.draw.polygon(window, red, triangulo2)
+    
+    Plano()
+    Masa()
 
-    # Dibujo del objeto (pelota)
-    pygame.draw.circle(window, green, (int(posicion_x), int(posicion_y)), 21)
-    pygame.draw.circle(window, red, (int(posicion_x2), int(posicion_y2)), 21)
+    posicion_y += (masa+fuerza+coeficiente_roce_dinamico+tiempo)-(coeficiente_roce_estatico+coeficiente_roce_dinamico)
+    posicion_x += (masa+fuerza+coeficiente_roce_dinamico+tiempo)-(coeficiente_roce_estatico+coeficiente_roce_dinamico)
+
+    
 
     # Actualizacion de la ventana
     pygame.display.flip()
     clock.tick(60)
+
+
+
+
+
+
+
 
 
 
